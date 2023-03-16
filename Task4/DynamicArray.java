227437801a7df3177df6676f9e5826b3cc28d669
rkorostin <rkorostin@gmail.com>
2023-03-16 13:55:26 +0300

    edit DynamicArray

--- Task4/DynamicArray.java
+++ Task4/DynamicArray.java
@@ -63,64 +63,8 @@ public class DynamicArray<T> {
         this.array = (T[]) new Object[10];
         this.size = 0;
     }
+
+
 }
 
-// public class DynamicArray<T> {
-// private T[] array;
 
-// public DynamicArray() {
-// this.array = (T[]) new Object[0];
-// }
-
-// public DynamicArray(T[] data) {
-// this.array = data;
-// }
-
-// public void append(T element) {
-// T[] newArray = (T[]) new Object[array.length + 1];
-// for (int i = 0; i < array.length; i++) {
-// newArray[i] = array[i];
-// }
-// newArray[array.length] = element;
-// array = newArray;
-// }
-
-// public T pop() {
-// T[] newArray = (T[]) new Object[array.length - 1];
-// T element = array[array.length - 1];
-// for (int i = 0; i < newArray.length; i++) {
-// newArray[i] = array[i];
-// }
-// array = newArray;
-// return element;
-// }
-
-// public T get(int index) {
-// return array[index];
-// }
-
-// public void set(int index, T value) {
-// array[index] = value;
-// }
-
-// public int size() {
-// return array.length;
-// }
-
-// public void clear() {
-// this.array = (T[]) new Object[0];
-// }
-
-// public String toString() {
-// StringBuilder stringBuilder = new StringBuilder();
-// stringBuilder.append("[");
-// for (int i = 0; i < array.length; i++) {
-// stringBuilder.append(array[i]);
-// if (i != array.length - 1) {
-// stringBuilder.append(", ");
-// }
-// }
-// stringBuilder.append("]");
-// return stringBuilder.toString();
-// }
-// }
