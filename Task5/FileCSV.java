553e4001655080ec12552dd72b1076eb700aac0a
rkorostin <rkorostin@gmail.com>
2023-03-19 18:50:59 +0300

    FileCSV - добавлено описание класса

--- Task5/FileCSV.java
+++ Task5/FileCSV.java
@@ -5,17 +5,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/*
+ Класс FileCSV содержит статические методы для чтения и записи данных в файл CSV. 
+ Методы writeToCSV() и readFromCSV() используются для записи и чтения данных в формате CSV соответственно.
+ Метод writeToCSV() принимает список контактов и имя файла в качестве аргументов. 
+ Он создает новый PrintWriter для записи данных в файл и формирует строку заголовка и строки данных в формате CSV. 
+ Затем он записывает эту строку в файл.
+ Метод readFromCSV() принимает имя файла в качестве аргумента и возвращает список контактов, прочитанных из файла. 
+ Он создает новый Scanner для чтения данных из файла, пропускает строку заголовка и затем читает каждую строку данных и создает новый объект Contact для каждой строки.
+ */
+
 public class FileCSV {
     public static void writeToCSV(List<Contact> contacts, String fileName) {
         try (PrintWriter writer = new PrintWriter(new File(fileName))) {
             StringBuilder sb = new StringBuilder();
-            sb.append("Имя, Фамилия, Телефон, email\n");
+            sb.append("Имя, Фамилия, Телефон\n");
 
             for (Contact contact : contacts) {
                 sb.append(contact.getFirstName()).append(",");
                 sb.append(contact.getLastName()).append(",");
-                sb.append(contact.getPhone()).append(",");
-                sb.append(contact.getEmail()).append("\n");
+                sb.append(contact.getPhone()).append("\n");
             }
 
             writer.write(sb.toString());
@@ -32,7 +41,7 @@ public class FileCSV {
 
             while (scanner.hasNextLine()) {
                 String[] fields = scanner.nextLine().split(",");
-                Contact contact = new Contact(fields[0], fields[1], fields[2], fields[3]);
+                Contact contact = new Contact(fields[0], fields[1], fields[2]);
                 contacts.add(contact);
             }
         } catch (FileNotFoundException e) {
