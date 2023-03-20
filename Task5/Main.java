b99c00f8b01d7cb8fd61c5379c1c6321b28b8eb8
rkorostin <rkorostin@gmail.com>
2023-03-20 19:25:29 +0300

    Оставил только метод start() для запуска UI

--- Task5/Main.java
+++ Task5/Main.java
@@ -2,8 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // создаём класс PhoneBook и передаём его в конструктор ConsoleView. 
-        // Затем вызваваем метод start() объекта ConsoleView, чтобы запустить интерфейс командной строки для работы с телефонной книгой.
+        // создаём класс PhoneBook и передаём его в конструктор ConsoleView.
+        // Затем вызваваем метод start() объекта ConsoleView, чтобы запустить интерфейс
+        // командной строки для работы с телефонной книгой.
         PhoneBook phoneBook = new PhoneBook();
         ConsoleView consoleView = new ConsoleView(phoneBook);
         consoleView.start();
