977646a19b0bb61ffce9dbd2e56c72c24b80e278
rkorostin <rkorostin@gmail.com>
2023-03-20 18:57:43 +0300

    ConsoleView. Добавил доп меню

--- Task5/ConsoleView.java
+++ Task5/ConsoleView.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 
 public class ConsoleView {
 
-    private PhoneBook addressBook;
+    private PhoneBook phoneBook;
     private Scanner scanner;
 
-    public ConsoleView(PhoneBook addressBook) {
-        this.addressBook = addressBook;
+    public ConsoleView(PhoneBook phoneBook) {
+        this.phoneBook = phoneBook;
         this.scanner = new Scanner(System.in);
     }
 
@@ -15,59 +15,79 @@ public class ConsoleView {
         boolean running = true;
         while (running) {
             System.out.println("Выберите действие: ");
-            System.out.println("1. Добавить контакт");
-            System.out.println("2. Удалить контакт");
-            System.out.println("3. Обновить контакт");
-            System.out.println("4. Поиск по телефону");
-            System.out.println("5. Импорт из CSV");
-            System.out.println("6. Экспорт в CSV");
-            System.out.println("7. Выход");
+            System.out.println("1. Посмотреть контакты");
+            System.out.println("2. Добавить контакт");
+            System.out.println("3. Удалить контакт");
+            System.out.println("4. Обновить контакт");
+            System.out.println("5. Поиск по телефону");
+            System.out.println("6. Поиск по имени");
+            System.out.println("7. Импорт из CSV");
+            System.out.println("8. Экспорт в CSV");
+            System.out.println("0. Выход");
 
             int choice = scanner.nextInt();
             scanner.nextLine(); 
 
             switch (choice) {
                 case 1:
-                    System.out.println("Введите email для поиска:");
-                    String email = scanner.nextLine();
-                    List<Contact> contacts = addressBook.searchByEmail(email);
-                    if (contacts.isEmpty()) {
-                        System.out.println("Контакт не найден");
-                    } else {
-                        for (Contact contact : contacts) {
-                            System.out.println(contact);
-                        }
-                    }
+                    phoneBook.displayContacts();
                     break;
                 case 2:
-                    System.out.println("Введите email для поиска:");
-                    String email = scanner.nextLine();
-                    List<Contact> contacts = addressBook.searchByEmail(email);
-                    if (contacts.isEmpty()) {
-                        System.out.println("Контакт не найден");
-                    } else {
-                        for (Contact contact : contacts) {
-                            System.out.println(contact);
-                        }
-                    }
+                    System.out.println("Введите имя: ");
+                    String firstName = scanner.nextLine();
+                    System.out.println("Введите фамилию: ");
+                    String lastName = scanner.nextLine();
+                    System.out.println("Введите телефон: ");
+                    String phone = scanner.nextLine();
+                    System.out.println("Введите id (любое число): ");
+                    String idContact = scanner.nextLine();
+                    Contact contact = new Contact(idContact, firstName, lastName, phone);
+                    phoneBook.addContact(contact);
                     break;
                 case 3:
-                    System.out.println("Введите имя файла:");
-                    String fileName = scanner.nextLine();
-                    addressBook.importFromCSV(fileName);
-                    System.out.println("Импортированные контакты из " + fileName);
-                    break;
-                case 3:
-                    System.out.println("Введите имя файла:");
-                    fileName = scanner.nextLine();
-                    addressBook.exportToCSV(fileName);
-                    System.out.println("Экспортированные контакты в " + fileName);
+                    System.out.println("Введите id удаляемого контакта: ");
+                    String idContactDelete = scanner.nextLine();
+                    phoneBook.removeContact(idContactDelete);
                     break;
                 case 4:
+                    System.out.println("Введите id обновляемого контакта: ");
+                    String idContactUpdate = scanner.nextLine();
+                    System.out.println("Введите новое имя: ");
+                    String newFirstName = scanner.nextLine();
+                    System.out.println("Введите новую фамилию: ");
+                    String newLastName = scanner.nextLine();
+                    System.out.println("Введите новый телефон: ");
+                    String newPhone = scanner.nextLine();
+                    phoneBook.updateContact(idContactUpdate, newFirstName, newLastName, newPhone);
+                    break;
+                case 5:
+                    System.out.println("Введите телефон для поиска:");
+                    String searchPhone = scanner.nextLine();
+                    phoneBook.searchByPhone(searchPhone);
+                    break;
+                case 6:
+                    System.out.println("Введите имя для поиска:");
+                    String searchName = scanner.nextLine();
+                    phoneBook.searchByName(searchName);
+                    break;
+                case 7:
+                    System.out.println("(Импорт данных) введите имя файла:");
+                    String fileName = scanner.nextLine();
+                    phoneBook.importFromCSV(fileName);
+                    System.out.println("Импортированные контакты из " + fileName);
+                    break;
+                case 8:
+                    System.out.println("(Экспорт данных) введите имя файла:");
+                    fileName = scanner.nextLine();
+                    phoneBook.exportToCSV(fileName);
+                    System.out.println("Экспортированные контакты в " + fileName);
+                    break;
+                case 0:
                     running = false;
                     break;
                 default:
                     System.out.println("Неверный выбор");
+                    
             }
         }
     }
